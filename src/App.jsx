90fd935554c5133228cdaa6df46c4fcf4dc3ccf9
Sequoia Nevae <sequoianevae@Sequoias-iMac.lan>
2024-04-15 22:10:29 -0500

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Routes, Route, Link, Router } from 'react-router-dom';
 import './App.css'
 import Movies from './components/Movies';
+import login from './components/login';
+import register from './components/register';
 
 function App() {
   const [] = useState()
@@ -11,11 +13,16 @@ function App() {
 
       <div id="container">
         <Link to='/'>Movies</Link>
+        <Link to='/login'>Login</Link>
+        <Link to='/register'>Register</Link>
+
       </div>
      
       <div>
         <Routes>
           <Route path='/' element={<Movies />} />
+          <Route path='/login' element={<login />} />
+          <Route path='/register' element={<register />} />
         </Routes>
       </div>
       
